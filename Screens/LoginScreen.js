81b872ad581c5a3a7183fcbc311fe4625136f01c
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-11 20:44:33 +0530

    screens

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
+import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
+  const { user } = useAuth(); //user is accessed from here because we wrapped Auth Provider around stack navigator, so that its available around whole app.
+  console.log(user);
   return (
     <View>
       <Text>LoginScreen</Text>
