2a4f0dff5d8545b4f7a7a2bdad29052892037400
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-07-11 20:44:16 +0530

    authentication

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
-import React, { useContext } from "react";
+import React, { createContext, useContext } from "react";
 
-const AuthContext = createContext({});
+const AuthContext = createContext({}); //create data layer, react context API
 
 export const AuthProvider = ({ children }) => {
+  //auth provider takes children which means the child elements.
   return (
     <AuthContext.Provider value={{ user: "Pooja" }}>
       {children}
@@ -13,3 +14,4 @@ export const AuthProvider = ({ children }) => {
 export default function useAuth() {
   return useContext(AuthContext);
 }
+//here useContext is a hook for making data accessible to whole app
