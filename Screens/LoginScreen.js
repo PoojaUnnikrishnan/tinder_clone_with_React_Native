bc0f9eaaa4a9749d5e2c46c4930c3b65f6e632d7
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-08-02 21:57:51 +0530

    screens

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,13 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
 import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
+  const { signInWithGoogle } = useAuth();
   const { user } = useAuth(); //user is accessed from here because we wrapped Auth Provider around stack navigator, so that its available around whole app.
   console.log(user);
   return (
     <View>
       <Text>LoginScreen</Text>
+      <Button title="login" onPress={signInWithGoogle}></Button>
     </View>
   );
 };
